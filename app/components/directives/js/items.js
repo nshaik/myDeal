fd6f4660f9c1209cdfea8ea260d0f386eab1d4ab
shaik <shaiknayeem786@gmail.com>
2015-12-09 18:19:38 +0530

    added directives

---
+++ app/components/directives/js/items.js
@@ -0,0 +1,9 @@
+angular.module('m_items',[])
+
+.controller('controller_items', ['$http', '$scope', function($http, $scope){
+	$http.get('api/items').success(function(result){
+		$scope.items=result;
+	}).error(function(e){
+
+	});
+}]);
